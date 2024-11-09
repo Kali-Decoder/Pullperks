6c7c3b83270b705c31d5bf2a77cc7c0a5d6969d9
marotipatre <vaibhavspatre@gmail.com>
2024-11-10 00:57:57 +0530

    changed button position

--- client/src/components/ContributorDistribution.tsx
+++ client/src/components/ContributorDistribution.tsx
@@ -235,7 +235,7 @@ export function ContributorDistribution({
           ))}
         </div>
 
-        <div className="mt-6 flex justify-end">
+        <div className="mt-6 flex justify-center">
           <button
             className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             onClick={async () => {
